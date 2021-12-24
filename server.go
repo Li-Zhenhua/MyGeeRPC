ab54839bfd1772be8a1609694696884b95e9d36e
Li <610136437@qq.com>
2021-12-24 18:53:01 +0800

    通过反射实现服务端的服务注册、服务发现功能

--- server.go
+++ server.go
@@ -3,11 +3,12 @@ package MyGeeRPC
 import (
 	"MyGeeRPC/codec"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -23,7 +24,9 @@ var DefaultOption = &Option{
 	CodecType: codec.GobType,
 }
 
-type Server struct{}
+type Server struct {
+	serviceMap sync.Map
+}
 
 func NewServer() *Server {
 	return &Server{}
@@ -96,6 +99,8 @@ func (server *Server) serveCodec(cc codec.Codec) {
 type request struct {
 	header       *codec.Header
 	argv, replyv reflect.Value
+	mtype        *methodType
+	svc          *service
 }
 
 func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
@@ -115,11 +120,24 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 		return nil, err
 	}
 	req := &request{header: header}
-	// TODO: 获取请求参数的类型
 	//先假设是简单的字符串
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
-		log.Println("rpc server: read argv err:", err)
+	//req.argv = reflect.New(reflect.TypeOf(""))
+
+	req.svc, req.mtype, err = server.findService(header.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+	req.argv = req.mtype.newArgv()
+	req.replyv = req.mtype.newReplyv()
+
+	//确保argvi是一个指针，因为ReadBody需要一个指针作为参数
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+
+	if err = cc.ReadBody(argvi); err != nil {
+		log.Println("rpc server: 读取Body信息时出错:", err)
 	}
 	return req, nil
 }
@@ -128,14 +146,51 @@ func (server *Server) sendResponse(cc codec.Codec, header *codec.Header, body in
 	sending.Lock()
 	defer sending.Unlock()
 	if err := cc.Write(header, body); err != nil {
-		log.Println("rpc server: write response error:", err)
+		log.Println("rpc server: 发送Response时出错:", err)
 	}
 }
 
 func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
-	// TODO: 使用真正的注册的方法处理请求
 	defer wg.Done()
-	log.Println(req.header, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("response : %d", req.header.Seq))
+	//log.Println(req.header, req.argv.Elem())
+	//req.replyv = reflect.ValueOf(fmt.Sprintf("response : %d", req.header.Seq))
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.header.Error = err.Error()
+		server.sendResponse(cc, req.header, invalidRequest, sending)
+		return
+	}
 	server.sendResponse(cc, req.header, req.replyv.Interface(), sending)
+}
+
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc: 服务此前已被成功注册: " + s.name)
+	}
+	return nil
+}
+
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server: 请求的方法名不合法！" + serviceMethod + "\n正确形式：service.method")
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	sevc, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server: 未找到相应的服务：" + serviceMethod)
+		return
+	}
+	svc = sevc.(*service) //断言，断言失败时会直接造成一个 panic
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server: 未找到相应的方法：" + serviceMethod)
+	}
+	return
 }
---
+++ service.go
@@ -0,0 +1,104 @@
+package MyGeeRPC
+
+import (
+	"go/ast"
+	"log"
+	"reflect"
+	"sync/atomic"
+)
+
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+func (m *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+	// arg may be a pointer type, or a value type
+	if m.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(m.ArgType.Elem())
+	} else {
+		argv = reflect.New(m.ArgType).Elem()
+	}
+	return argv
+}
+
+func (m *methodType) newReplyv() reflect.Value {
+	// reply在这里一定是一个指针
+	replyv := reflect.New(m.ReplyType.Elem())
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	}
+	return replyv
+}
+
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+func newService(rcvr interface{}) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	//当v是指针时，Indirect(v Value)函数用于获取v指向的值
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	//fmt.Println(reflect.Indirect(s.rcvr).Type()) -> MyGeeRPC.Foo
+	//fmt.Println(s.rcvr.Type())                   -> *MyGeeRPC.Foo
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc服务端出错: %s 是不合法的名称", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc服务端method注册成功: %s.%s\n", s.name, method.Name)
+	}
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	//IsExported()就是判断方法是不是能导出的，也就是是不是以大写字母开头的
+	//像string，int这些build-in类型，PkgPath（包路径）为空
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
